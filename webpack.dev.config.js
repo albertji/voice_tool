cb9a10fbf161a427496167eb0716193fc6bc154d
alber ji <jihaining627482592@163.com>
2018-11-03 11:27:16 +0800

    加上gitignore和gitattributes配置，修改eslink规则

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,15 +1,15 @@
 const path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  /*入口*/
+  /* 入口 */
   entry: ['react-hot-loader/patch', path.join(__dirname, 'src/index.js')],
 
-  /*输出到dist文件夹，输出文件名字为bundle.js*/
+  /* 输出到dist文件夹，输出文件名字为bundle.js */
   output: {
     path: path.join(__dirname, './dist'),
-    //path: 'C:\\wamp64\\www',
+    // path: 'C:\\wamp64\\www',
     filename: '[name].[hash].js',
     chunkFilename: '[name].[chunkhash].js',
     publicPath: '/'
@@ -45,7 +45,7 @@ module.exports = {
       filename: 'index.html',
       template: path.join(__dirname, 'src/index.html')
     })
-    //new UglifyJSPlugin()
+    // new UglifyJSPlugin()
   ],
   devServer: {
     port: 3344,
